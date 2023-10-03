ef2fddb765012a8a01d1208fe7cda161564b1b2e
Angelo Nicolo <noreply>
2023-10-03 13:32:56 -0700

    update

---
+++ week1/Introduce yourself/script.js
@@ -0,0 +1,4 @@
+var firstName = "Angelo";
+var lastName = "Nicolo";
+var yearsOfStudy = 1; // Update with your years of study
+var goal = "To find a job related to software Development."; // Update with your goal
